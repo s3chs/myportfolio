df22f4c47182353148da1090b7644d8909728fec
sechs <philippe.buteau@skema.edu>
2020-11-06 18:43:11 +0100

    debugged the blue color on nav, ready to make it responsive and deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={WelcomePage} />
-          <Route exact path="/myportfolio/" component={MainPage}/>
+          <Route exact path="/home" component={MainPage}/>
         </Switch>
       </BrowserRouter>
     </div>
